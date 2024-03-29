7096c893166e5d8095fb4191a9b55d7e9700b699
AIlhomov <ilhomovvahid@gmail.com>
2024-03-29 21:04:25 +0100

    solved: basketballoneonone

---
+++ basketballoneonone.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string score;
+
+    getline(cin, score);
+
+    cout << score[score.length() - 2] << endl;
+
+    return 0;
+}
