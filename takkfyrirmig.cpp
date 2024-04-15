90c96ad9276950ba3ae0d2e6cf2ba515a9261c1c
AIlhomov <ilhomovvahid@gmail.com>
2024-04-15 16:53:38 +0200

    solved: takkfyrirmig

---
+++ takkfyrirmig.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define inf 1e9
+#define quit cout << endl; exit(0);
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while (t--){
+        string name;
+        cin >> name;
+        cout << "Takk " << name << endl;
+    }
+
+
+    quit;
+}
