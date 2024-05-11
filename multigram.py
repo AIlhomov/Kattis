4f626f8e1a91032bf9183869eb0aa7ae2febd207
AIlhomov <ilhomovvahid@gmail.com>
2024-05-11 13:36:44 +0200

    solved: multigram

---
+++ multigram.py
@@ -0,0 +1,16 @@
+def multigram(s):
+    for i in range(1, len(s)):
+        if len(s) % i == 0:
+            root = s[:i]
+            is_multigram = True
+            for j in range(i, len(s), i):
+                if sorted(s[j:j+i]) != sorted(root):
+                    is_multigram = False
+                    break
+            if is_multigram:
+                return root
+    return -1
+
+s = input()
+
+print(multigram(s))
